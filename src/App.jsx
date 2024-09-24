519b9f5cc54c625435fe5e1689290c413be12078
Arslan Javaid <aja118379@gmail.com>
2024-09-24 13:08:45 +0500

    Header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-green-400'>Portfolio</h1>
-      <h2 className='bg-red-400'>Portfolio2</h2>
+      <Header />
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ['Space Mono', 'monospace'],  // For Space Mono
+        fira: ['Fira Sans', 'sans-serif'],  // For Fira Sans
+      },
+      colors: {
+        primary: '#387FC8',  // Color 1 - Blue
+        primary2: '#295787',  // Color 2 - Dark Blue
+        secondary: '#F3B552',  // Color 2 - Yellow
+        dark: '#393943',  // Color 3 - Dark Gray
+      },
+    },
   },
   plugins: [],
 }
