12927021c5ccc7a2bff9774326654da7c9c4903f
Arslan Javaid <aja118379@gmail.com>
2024-09-24 12:27:18 +0500

    Innitial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400'>Portfolio</h1>
+      <h2 className='bg-red-400'>Portfolio2</h2>
+    </>
+  )
+}
+
+export default App
