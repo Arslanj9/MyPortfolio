036eb56c9ea9a4c2fed1e81834b81f9c8117da7b
Arslan Javaid <aja118379@gmail.com>
2024-09-25 10:40:35 +0500

    Some little adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,15 @@ import Button from './Button/Button'
 const Header = () => {
     return (
         <header className="min-h-screen flex flex-col justify-center items-center p-6">
-            <div className="text-center">
+            <div className="text-center max-w-[460px]">
               
-                <p className="text-sm font-mono text-amber-400">Hi, my name is</p>
+                <p className="text-[0.5rem] font-mono text-amber-400">Hi, my name is</p>
 
-                <h1 className="text-4xl font-bold text-sky-500 mt-2 font-fira">Arslan Javaid</h1>
+                <h1 className="text-4xl font-bold text-sky-100 mt-2 font-fira">Arslan Javaid</h1>
 
-                <h1 className="text-2xl font-bold text-sky-700">Full Stack Developer</h1>
+                <h1 className="text-xl font-medium text-sky-300">Full Stack Developer</h1>
 
-                <p className="text-sm text-gray-600 mt-4">
+                <p className="text-[0.65rem] text-sky-300 mt-3 ">
                     I specialize in MERN stack development, building responsive web applications
                     using MongoDB, Express.js, React, and Node.js.
                 </p>
